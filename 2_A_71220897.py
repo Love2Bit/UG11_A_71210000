1d677b0d20690396c6e1d4db99696f5ad99e3693
JustAo <51353623+Love2Bit@users.noreply.github.com>
2022-12-02 10:14:05 +0700

    Add files via upload

--- 2_A_71220897.py
+++ 2_A_71220897.py
@@ -1,9 +1,11 @@
-def cetakHuruf(a):
-    if left == 0 :
-        print(a[b==0:b==3])
-    else :
-        pass
+def cetakHuruf(str):
+    strLength = len(str)
+    if strLength % 2 == 0 :
+        return str[0] + str[1] + str[2] + ' dan ' + str[-3] + str[-2] + str[-1]
+    else:
+        tengah = int(strLength / 2)
+        return str[tengah - 1] + str[tengah] + str[tengah + 1]
 
-a = input('Masukan kata : ')
-b = len(a)
-left = b % 2
+kata = input('Masukan kata : ')
+out = cetakHuruf(kata)
+print('Huruf yang diambil pada kata', kata, 'adalah', out)
